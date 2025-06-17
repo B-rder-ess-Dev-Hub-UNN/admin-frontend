d9577937632ae529a9f3c024041cc5f689507fcc
tochukwu <okeakputochukwu9@gmail.com>
2025-06-17 16:27:08 -0400

    Finish admin frontend UI

--- src/pages/SignIn.tsx
+++
@@ -1,8 +0,0 @@
-export default function SignIn() {
-  return (
-    <div className="flex flex-row w-[305px] h-[32px]">
-      <img src="logo.png" className="w-[59px] h-[13px]"></img>
-      <p>Tech club UNN</p>
-    </div>
-  );
-}
